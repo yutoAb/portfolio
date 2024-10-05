8baf79618dac388c2a6994fb7d99a3492ceada61
{yutoAb} <{yutoabe0207}>
2024-10-05 16:40:31 +0900

    Topコンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Top } from './Top/Top';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Top/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Top/Top.tsx
@@ -0,0 +1,3 @@
+export const Top = () => {
+    return <div>TopPage</div>
+}
