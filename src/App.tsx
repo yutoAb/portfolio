4dfbd950d23072d06b50523d135883ac4c0a58df
{yutoAb} <{yutoabe0207}>
2024-10-06 12:35:45 +0900

    タブ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import Reac, { useRef } from "react";
 import logo from "./logo.svg";
 import { Top } from "./Top/Top";
 import { Project } from "./Project/Project";
-import { Button } from "@mui/material";
+import { Tabs, Tab, Box, Button } from "@mui/material";
 
 const App = () => {
+  const topRef = useRef<HTMLDivElement | null>(null);
+  const projectRef = useRef<HTMLDivElement | null>(null);
+
+  const handleScroll = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      window.scrollTo({
+        top: ref.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <Top />
-      <Project />
+      <Tabs>
+        <Tab label="Top" onClick={() => handleScroll(topRef)} />
+        <Tab label="Project" onClick={() => handleScroll(projectRef)} />
+      </Tabs>
+      <div ref={topRef}>
+        <Top />
+      </div>
+      <div ref={projectRef}>
+        <Project />
+      </div>
       <Button>text</Button>
     </div>
   );
--- src/Top/Top.tsx
+++ src/Top/Top.tsx
@@ -1,5 +1,55 @@
 import { Typography } from "@mui/material";
 
 export const Top = () => {
-  return <Typography variant="h2">Yuto Abe / 阿部 雄斗</Typography>;
+  return (
+    <>
+      <Typography variant="h2"> Yuto Abe / 阿部 雄斗</Typography>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+      <div>私は阿部です私は阿部です</div>
+    </>
+  );
 };
