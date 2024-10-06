2d281c62cb7a9bce3aea00c79d31c314dd02934f
{yutoAb} <{yutoabe0207}>
2024-10-06 12:40:15 +0900

    タブ部分の作成

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,39 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <Tabs>
-        <Tab label="Top" onClick={() => handleScroll(topRef)} />
-        <Tab label="Project" onClick={() => handleScroll(projectRef)} />
-      </Tabs>
-      <div ref={topRef}>
-        <Top />
-      </div>
-      <div ref={projectRef}>
-        <Project />
-      </div>
-      <Button>text</Button>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          height: "100vh",
+          width: "200px",
+          borderRight: 1,
+          borderColor: "divider",
+          bgcolor: "background.paper",
+        }}
+      >
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          sx={{ borderRight: 1, borderColor: "divider", width: "200px" }}
+        >
+          <Tab label="Top" onClick={() => handleScroll(topRef)} />
+          <Tab label="Project" onClick={() => handleScroll(projectRef)} />
+        </Tabs>
+      </Box>
+
+      <Box sx={{ marginLeft: "200px", padding: 2, width: "100%" }}>
+        <div ref={topRef}>
+          <Top />
+        </div>
+        <div ref={projectRef}>
+          <Project />
+        </div>
+        <Button>text</Button>
+      </Box>
+    </Box>
   );
 };
 
