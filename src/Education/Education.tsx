742a2a2715c0186b0d28fc8a7c6f16edb474b61b
{yutoAb} <{yutoabe0207}>
2024-10-06 17:10:47 +0900

    背景色追加

--- src/Education/Education.tsx
+++ src/Education/Education.tsx
@@ -1,9 +1,18 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 export const Education = () => {
   return (
-    <>
-      <Typography variant="h3">Education</Typography>
+    <Box
+      sx={{
+        backgroundColor: "rgb(235, 148, 207)",
+        padding: "20px",
+        color: "white",
+      }}
+    >
+      <Typography variant="h3" sx={{ paddingTop: "100px" }}>
+        Education
+      </Typography>
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
@@ -37,29 +46,6 @@ export const Education = () => {
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-    </>
+    </Box>
   );
 };
--- src/Top/Top.tsx
+++ src/Top/Top.tsx
@@ -1,8 +1,14 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 export const Top = () => {
   return (
-    <>
+    <Box
+      sx={{
+        backgroundColor: "rgb(94, 66, 166)",
+        padding: "20px",
+        color: "white",
+      }}
+    >
       <Typography variant="h2"> Yuto Abe / 阿部 雄斗</Typography>
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
@@ -26,30 +32,6 @@ export const Top = () => {
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
       <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-    </>
+    </Box>
   );
 };
