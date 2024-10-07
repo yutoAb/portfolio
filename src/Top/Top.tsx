261aee95c806e8e2e140ca2387d9e29dd3593e8d
{yutoAb} <{yutoabe0207}>
2024-10-07 16:45:29 +0900

    ペンギンの画像処理

--- src/Top/Top.tsx
+++ src/Top/Top.tsx
@@ -1,5 +1,6 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Stack } from "@mui/material";
 import { purple } from "../Constants";
+import penguin from "../assets/penguin.png";
 
 export const Top = () => {
   return (
@@ -10,29 +11,55 @@ export const Top = () => {
         color: "white",
       }}
     >
-      <Typography variant="h2" sx={{ paddingTop: "100px" }}> Yuto Abe / 阿部 雄斗</Typography>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
+      <Stack direction="row" spacing={2}>
+        <Box
+          component="img"
+          src={penguin}
+          alt="penguin"
+          sx={{
+            width: "200px",
+            height: "auto",
+            objectFit: "contain",
+          }}
+        />
+        <Box>
+          <Typography variant="h2" sx={{ paddingTop: "100px" }}>
+            Yuto Abe / 阿部 雄斗
+          </Typography>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+          <div>私は阿部です私は阿部です</div>
+        </Box>
+      </Stack>
     </Box>
   );
 };
