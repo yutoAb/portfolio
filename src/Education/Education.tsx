8b05ba13026a851c5ce6be835c7e7912ee1e954d
{yutoAb} <{yutoabe0207}>
2024-10-07 18:36:09 +0900

    Education追加

--- src/Education/Education.tsx
+++ src/Education/Education.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Stack } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { pink } from "../Constants";
 
@@ -9,39 +9,36 @@ export const Education = () => {
         backgroundColor: pink,
         padding: "20px",
         color: "white",
-        minHeight: "100vh", 
+        minHeight: "100vh",
       }}
     >
       <Typography variant="h3" sx={{ paddingTop: "100px" }}>
         Education
       </Typography>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
-      <div>私は阿部です私は阿部です</div>
+      <Stack direction="row" spacing={10}>
+        <Box>
+          <Box>
+            <div>学歴</div>
+            <div>早稲田大学</div>
+            <div>2021年4月～現在　|　基幹理工学部 情報通信学科</div>
+            <div>2024年4月～現在　|　小林・小川研究室</div>
+          </Box>
+          <Box>
+            <div>スキル</div>
+            <div>WEBアプリケーション制作</div>
+            <div>フロント : React × TypeScript</div>
+            <div>バック : Scala</div>
+            <div>機械学習</div>
+            <div>Python</div>
+            <div>ツール : ESPnet</div>
+          </Box>
+        </Box>
+        <Box>
+          <div>職歴</div>
+          <div>2022年10月～2023年1月　|　キャロットソフトウェア株式会社</div>
+          <div>2023年2月～現在　|　日本ユースウェアシステム株式会社</div>
+        </Box>
+      </Stack>
     </Box>
   );
 };
