9f3baedd745f8d8f6161a001155a8583de7fce6d
{yutoAb} <{yutoabe0207}>
2025-02-13 01:17:06 +0900

    不要な左側のタブを削除

--- src/MobileContents.tsx
+++ src/MobileContents.tsx
@@ -54,77 +54,21 @@ const MobileContents = () => {
   }, []);
 
   return (
-    <Stack direction="row">
-      {/* 左側のタブ */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          height: "100vh",
-          width: isTabsMinimized ? "10px" : "200px",
-          borderRight: 1,
-          borderColor: "divider",
-          bgcolor: "background.paper",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          transition: "width 0.3s",
-        }}
-      >
-        <IconButton
-          onClick={() => setIsTabsMinimized(!isTabsMinimized)}
-          sx={{ position: "absolute", top: 10, right: -25 }}
-        >
-          {isTabsMinimized ? <ChevronRight /> : <ChevronLeft />}
-        </IconButton>
-        {!isTabsMinimized && (
-          <Tabs
-            orientation="vertical"
-            variant="scrollable"
-            value={activeTab}
-            sx={{
-              borderRight: 1,
-              borderColor: "divider",
-              width: "100%",
-            }}
-          >
-            <Tab label="Top" onClick={() => handleScroll(topRef, 0)} />
-            <Tab label="Project" onClick={() => handleScroll(projectRef, 1)} />
-            <Tab
-              label="Education"
-              onClick={() => handleScroll(educationRef, 2)}
-            />
-            <Tab label="Contact" onClick={() => handleScroll(contactRef, 3)} />
-          </Tabs>
-        )}
-      </Box>
-
-      {/* 右側のコンテンツ */}
-      <Box
-        sx={{
-          marginLeft: isTabsMinimized ? "50px" : "200px", // タブの幅分をマージンに設定
-          padding: 2,
-          width: isTabsMinimized ? "calc(100% - 50px)" : "calc(100% - 200px)", // タブの幅を引いたサイズに調整
-          transition: "margin-left 0.3s, width 0.3s",
-        }}
-      >
-        <div ref={topRef} style={{ minHeight: "100vh" }}>
-          <Top />
-        </div>
-        <div ref={projectRef} style={{ minHeight: "100vh" }}>
-          <Project />
-        </div>
-        <div ref={educationRef} style={{ minHeight: "100vh" }}>
-          <Education />
-        </div>
-        <div ref={contactRef} style={{ minHeight: "100vh" }}>
-          <Contact />
-        </div>
-        <Button onClick={() => handleScroll(topRef, 0)}>トップへ</Button>
-      </Box>
-    </Stack>
+    <Box>
+      <div ref={topRef} style={{ minHeight: "100vh" }}>
+        <Top />
+      </div>
+      <div ref={projectRef} style={{ minHeight: "100vh" }}>
+        <Project />
+      </div>
+      <div ref={educationRef} style={{ minHeight: "100vh" }}>
+        <Education />
+      </div>
+      <div ref={contactRef} style={{ minHeight: "100vh" }}>
+        <Contact />
+      </div>
+      <Button onClick={() => handleScroll(topRef, 0)}>トップへ</Button>
+    </Box>
   );
 };
 
