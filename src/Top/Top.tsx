6a741a3724ac14c972b9619e4a2cd726c84da261
{yutoAb} <{yutoabe0207}>
2025-02-12 15:51:31 +0900

    トップページ文章作成

--- src/Top/Top.tsx
+++ src/Top/Top.tsx
@@ -25,25 +25,29 @@ export const Top = () => {
             objectFit: "contain",
           }}
         />
-        <Stack spacing={2}>
+        <Stack spacing={5}>
           <Typography variant="h2" sx={{ paddingTop: "100px" }}>
             Yuto Abe / 阿部 雄斗
           </Typography>
           <Box>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
-            <div>私は阿部です私は阿部です</div>
+            <div>皆さん初めまして．阿部 雄斗（アベ ユウト）と申します．</div>
+            <br/>
+            <div>
+              早稲田大学 情報通信学科の小林・小川研究室に所属させていただいている修士１年生です．
+            </div>
+            <div>
+              研究内容としては，機械学習を用いた音声認識モデルの精度向上を目指して取り組んでいます．
+            </div>
+            <br/>
+            <div>
+              また長期インターンでは日本ユースウェアシステム株式会社で，WEBアプリの開発をさせていただいています．
+            </div>
+            <div>
+              フロントでは React×TypeScript，バックでは Scalaを触っています．
+            </div>
+            <br />
+            <div>趣味はマンガとゴルフです．</div>
+            <div>不格好なホームページですが，人となりを知っていただければ幸いです．</div>
           </Box>
         </Stack>
       </Stack>
