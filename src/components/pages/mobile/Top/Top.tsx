c5f225c6c2923974ffdd08d55f44126caab5f0c1
{yutoAb} <{yutoabe0207}>
2025-02-16 15:57:48 +0900

    調整

--- src/components/pages/mobile/Top/Top.tsx
+++ src/components/pages/mobile/Top/Top.tsx
@@ -14,45 +14,45 @@ export const Top = () => {
         // justifyContent: "center",
       }}
     >
-      <Stack>
-        <Box
-          component="img"
-          src={penguin}
-          alt="penguin"
-          sx={{
-            width: "400px",
-            height: "auto",
-            objectFit: "contain",
-          }}
-        />
-        <Stack spacing={5}>
-          <Typography variant="h2" sx={{ paddingTop: "100px" }}>
-            Yuto Abe / 阿部 雄斗
+      <Stack spacing={6}>
+        <Stack direction="row" spacing={3}>
+          <Box
+            component="img"
+            src={penguin}
+            alt="penguin"
+            sx={{
+              width: "40%",
+              height: "auto",
+              objectFit: "contain",
+            }}
+          />
+          <Typography variant="h3">
+            Yuto Abe <br /> 阿部 雄斗
           </Typography>
-          <Box>
-            <div>皆さん初めまして．阿部 雄斗（アベ ユウト）と申します．</div>
-            <br />
-            <div>
-              早稲田大学
-              情報通信学科の小林・小川研究室に所属させていただいている修士１年生です．
-            </div>
-            <div>
-              研究内容としては，機械学習を用いた音声認識モデルの精度向上を目指して取り組んでいます．
-            </div>
-            <br />
-            <div>
-              また長期インターンでは日本ユースウェアシステム株式会社で，WEBアプリの開発をさせていただいています．
-            </div>
-            <div>
-              フロントでは React×TypeScript，バックでは Scalaを触っています．
-            </div>
-            <br />
-            <div>趣味はマンガとゴルフです．</div>
-            <div>
-              不格好なホームページですが，人となりを知っていただければ幸いです．
-            </div>
-          </Box>
         </Stack>
+        <Box>
+          <div>皆さん初めまして．阿部 雄斗（アベ ユウト）と申します．</div>
+          <br />
+          <div>
+            早稲田大学
+            情報通信学科の小林・小川研究室に所属させていただいている修士１年生です．
+          </div>
+          <div>
+            研究内容としては，機械学習を用いた音声認識モデルの精度向上を目指して取り組んでいます．
+          </div>
+          <br />
+          <div>
+            また長期インターンでは日本ユースウェアシステム株式会社で，WEBアプリの開発をさせていただいています．
+          </div>
+          <div>
+            フロントでは React×TypeScript，バックでは Scalaを触っています．
+          </div>
+          <br />
+          <div>趣味はマンガとゴルフです．</div>
+          <div>
+            不格好なホームページですが，人となりを知っていただければ幸いです．
+          </div>
+        </Box>
       </Stack>
     </Box>
   );
