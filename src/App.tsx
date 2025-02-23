47e059b15b6f5c90817394f51e06eff7cad5c7a1
{yutoAb} <{yutoabe0207}>
2025-02-23 18:56:59 +0900

    react-router-dom の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { MediaQueryProvider } from "./components/pages/common/Provider/MediaQueryProvider";
 import MainComponent from "./components/pages/MainComponent";
+import { BrowserRouter } from "react-router-dom";
+
+/**
+ * サーバーサイドのコンテキストパス
+ */
+export const ENDPOINT_BASE_PATH = `/portfolio`;
 
 export default function App() {
   return (
-    <MediaQueryProvider>
-      <MainComponent />
-    </MediaQueryProvider>
+    <BrowserRouter basename={`${ENDPOINT_BASE_PATH}/app`}>
+      <MediaQueryProvider>
+        <MainComponent />
+      </MediaQueryProvider>
+    </BrowserRouter>
   );
 }
