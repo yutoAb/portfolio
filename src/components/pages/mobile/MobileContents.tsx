e37dbd80273dd83b9b48ac79a3632eb84800dfee
{yutoAb} <{yutoabe0207}>
2025-02-24 15:04:38 +0900

    リファクタリング

--- src/components/pages/mobile/MobileContents.tsx
+++ src/components/pages/mobile/MobileContents.tsx
@@ -1,14 +1,11 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import { Top } from "./Top/Top";
 import { Project } from "./Project/Project";
 import { Education } from "./Education/Education";
 import { Contact } from "./Contact/Contact";
-import { Tabs, Tab, Box, Button, Stack, IconButton } from "@mui/material";
-import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { Box, Button } from "@mui/material";
 
 const MobileContents = () => {
-  const [activeTab, setActiveTab] = useState(0);
-  const [isTabsMinimized, setIsTabsMinimized] = useState(false);
   const topRef = useRef<HTMLDivElement | null>(null);
   const projectRef = useRef<HTMLDivElement | null>(null);
   const educationRef = useRef<HTMLDivElement | null>(null);
@@ -23,35 +20,8 @@ const MobileContents = () => {
         top: ref.current.offsetTop,
         behavior: "smooth",
       });
-      setActiveTab(index);
     }
   };
-
-  const handleScrollEvent = () => {
-    const topPosition = topRef.current?.offsetTop || 0;
-    const projectPosition = projectRef.current?.offsetTop || 0;
-    const educationPosition = educationRef.current?.offsetTop || 0;
-    const contactPosition = contactRef.current?.offsetTop || 0;
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition >= contactPosition - 100) {
-      setActiveTab(3);
-    } else if (scrollPosition >= educationPosition - 100) {
-      setActiveTab(2);
-    } else if (scrollPosition >= projectPosition - 100) {
-      setActiveTab(1);
-    } else {
-      setActiveTab(0);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScrollEvent);
-
-    return () => {
-      window.removeEventListener("scroll", handleScrollEvent);
-    };
-  }, []);
 
   return (
     <Box sx={{ width: "100%", margin: 0, padding: 0, overflowX: "hidden" }}>
