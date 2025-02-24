f497c6760729a53cf381fdba7c8dcc8b189ffc4f
{yutoAb} <{yutoabe0207}>
2025-02-24 18:34:52 +0900

    NotFound 追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { MediaQueryProvider } from "./components/pages/common/Provider/MediaQueryProvider";
 import MainComponent from "./components/pages/MainComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NotFound } from "./components/pages/NotFound";
 
+/**
+ * サーバーサイドのコンテキストパス
+ */
+export const ENDPOINT_BASE_PATH = ``;
 export default function App() {
   return (
-    <HelmetProvider>
-      <MediaQueryProvider>
-        <MainComponent />
-      </MediaQueryProvider>
-    </HelmetProvider>
+    <BrowserRouter basename={`${ENDPOINT_BASE_PATH}`}>
+      <HelmetProvider>
+        <MediaQueryProvider>
+          <Routes>
+            <Route path="">
+              <Route index element={<MainComponent />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </MediaQueryProvider>
+      </HelmetProvider>
+    </BrowserRouter>
   );
 }
