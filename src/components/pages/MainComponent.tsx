3ce17530fadca31972bc9240f716382bbf70f182
{yutoAb} <{yutoabe0207}>
2025-02-24 12:18:36 +0900

    ページタイトルの変更

--- src/components/pages/MainComponent.tsx
+++ src/components/pages/MainComponent.tsx
@@ -2,12 +2,16 @@ import React, { FC } from "react";
 import MobileContents from "./mobile/MobileContents";
 import PcContents from "./pc/PcContents";
 import { useMediaQueryContext } from "./common/Provider/MediaQueryProvider";
+import { Helmet } from "react-helmet-async";
 
 const MainComponent: FC = () => {
   const { isMobileSite, isTabletSite, isPcSite } = useMediaQueryContext();
 
   return (
     <>
+      <Helmet>
+        <title>yutoAb</title>
+      </Helmet>
       {isMobileSite && <MobileContents />}
       {/* {isTabletSite && <TabletContents />} */}
       {isTabletSite && <PcContents />}
