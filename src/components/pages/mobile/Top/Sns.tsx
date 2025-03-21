c24a3b64798a7f1e07700b8689371cca177ce703
{yutoAb} <{yutoabe0207}>
2025-03-21 16:32:29 +0900

    Linkedin 追加

--- src/components/pages/mobile/Top/Sns.tsx
+++ src/components/pages/mobile/Top/Sns.tsx
@@ -3,16 +3,16 @@ import { Stack, Box } from "@mui/material";
 import instagram from "../../../../assets/Instagram_Glyph_White.png";
 import x from "../../../../assets/logo-white.png";
 import github from "../../../../assets/github-mark-white.png";
-
+import linkin from "../../../../assets/InBug-White.png";
 export const Sns = () => {
   return (
-    <Stack spacing={2} direction={"row"}>
+    <Stack spacing={2} direction={"row"} alignItems={"center"}>
       <Box
         component="a"
         href="https://www.instagram.com/yuto_abe_"
         target="_blank"
         rel="noopener noreferrer"
-        sx={{ width: "18%" }}
+        sx={{ width: "15%" }}
       >
         <Box
           component="img"
@@ -30,7 +30,7 @@ export const Sns = () => {
         href="https://twitter.com/ReactYuto"
         target="_blank"
         rel="noopener noreferrer"
-        sx={{ width: "18%" }}
+        sx={{ width: "13%" }}
       >
         <Box
           component="img"
@@ -48,12 +48,30 @@ export const Sns = () => {
         href="https://github.com/yutoAb"
         target="_blank"
         rel="noopener noreferrer"
-        sx={{ width: "18%" }}
+        sx={{ width: "15%" }}
       >
         <Box
           component="img"
           src={github}
           alt="github"
+          sx={{
+            width: "100%",
+            height: "auto",
+            objectFit: "contain",
+          }}
+        />
+      </Box>
+      <Box
+        component="a"
+        href="https://linkedin.com/in/yutoAb"
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{ width: "15%" }}
+      >
+        <Box
+          component="img"
+          src={linkin}
+          alt="linkin"
           sx={{
             width: "100%",
             height: "auto",
