a24a30d494e95789f061ab2972a437873da79134
{yutoAb} <{yutoabe0207}>
2025-07-26 19:19:42 +0900

    プロジェクトページにポートフォリオセクションを追加し、ダイアログ表示のロジックを実装

--- src/components/pages/mobile/Project/Project.tsx
+++ src/components/pages/mobile/Project/Project.tsx
@@ -1,8 +1,21 @@
 import { Typography, Box, Stack } from "@mui/material";
 import { blue } from "../../common/Constants";
 import movie from "../../../../assets/卒業論文研究紹介動画.mp4";
+import { portfolioItems, PortfolioItem } from "../../pc/Project/PortfolioItem";
+import { useState } from "react";
+import { interactiveBoxStyle } from "../../pc/Project/style";
+import { SimpleDialog } from "../../pc/Project/SimpleDialog";
 
 export const Project = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedApp, setSelectedApp] = useState<PortfolioItem | null>(null);
+
+  const handleOpen = (item: PortfolioItem) => {
+    setSelectedApp(item);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
   return (
     <Box
       sx={{
@@ -24,8 +37,36 @@ export const Project = () => {
             卒業論文
           </Typography>
           <video src={movie} controls style={{ width: "100%" }} />
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: "bold", marginBottom: "10px" }}
+          >
+            ポートフォリオ
+          </Typography>
+
+          <Stack spacing={2}>
+            {portfolioItems.map((item, index) => (
+              <Box
+                key={index}
+                onClick={() => handleOpen(item)}
+                sx={interactiveBoxStyle}
+              >
+                {item.title}
+              </Box>
+            ))}
+          </Stack>
         </Stack>
       </Box>
+
+      {selectedApp && (
+        <SimpleDialog
+          open={open}
+          handleClose={handleClose}
+          title={selectedApp.title}
+          content={selectedApp.description}
+          link={selectedApp.link}
+        />
+      )}
     </Box>
   );
 };
